873115fd92d9b9310af9c5f2f60f4a1f47fb0236
imran.khan <imran.khan@ebaotech.com>
2020-03-05 11:52:24 +1100

    Create from Node.js Express JS Template

---
+++ config/config.js
@@ -0,0 +1,12 @@
+exports = module.exports = {
+    port: process.env.PORT || 8080,
+    api_info: {
+        title: "imran-test-service-2",
+        version: "1.0",
+        description: "API to do something...",
+        termsOfServiceUrl: "",
+        contact: "yourname@something.com",
+        license: "",
+        licenseUrl: ""
+    }
+};
